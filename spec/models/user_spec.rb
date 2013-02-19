26a6868b6d0cbf8a4758c3486cc7914c86f7b7c5
dolimite <sloftis@gmail.com>
2013-02-19 12:09:42 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -65,7 +67,7 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when password doesn't mathc confirmation" do
+  describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
@@ -82,6 +84,7 @@ describe User do
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
+  
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
@@ -93,6 +96,10 @@ describe User do
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
+  end
+  describe "remember token" do 
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
 
